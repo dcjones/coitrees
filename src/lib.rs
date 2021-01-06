65c0439132e95e96c9b59441d7758b9f07f307f4
Daniel C. Jones <dcjones@cs.washington.edu>
2021-01-05 17:49:22 -0800

    Make query work with avx.

--- src/lib.rs
+++ src/lib.rs
@@ -237,6 +237,66 @@ impl<T, I> IntervalNode<T, I> where T: Clone, I: IntWithMax {
         }
     }
 
+    fn query_chunk_metadata<'a, F>(&'a self, query_first: i32x8, query_last: i32x8, mut visit: F)
+            where F: FnMut(i32, i32, &'a T) {
+
+        let cmp = unsafe {
+            let cmp1 = _mm256_cmpgt_epi32(query_last, self.firsts);
+            let cmp2 = _mm256_cmpgt_epi32(self.lasts, query_first);
+            _mm256_movemask_epi8(_mm256_and_si256(cmp1, cmp2)) };
+
+        // could be made nicer with a macro perhaps?
+        let masks = [0xf, 0xf0, 0xf00, 0xf000, 0xf0000, 0xf00000, 0xf000000, 0xf000000];
+        if cmp & masks[0] != 0 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 0) },
+                unsafe { _mm256_extract_epi32(self.lasts, 0) },
+                &self.metadata[0] );
+        }
+        if cmp & masks[1] != 0 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 1) },
+                unsafe { _mm256_extract_epi32(self.lasts, 1) },
+                &self.metadata[1] );
+        }
+        if cmp & masks[2] != 0 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 2) },
+                unsafe { _mm256_extract_epi32(self.lasts, 2) },
+                &self.metadata[2] );
+        }
+        if cmp & masks[3] != 0 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 3) },
+                unsafe { _mm256_extract_epi32(self.lasts, 3) },
+                &self.metadata[3] );
+        }
+        if cmp & masks[4] != 0 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 4) },
+                unsafe { _mm256_extract_epi32(self.lasts, 4) },
+                &self.metadata[4] );
+        }
+        if cmp & masks[5] != 0 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 5) },
+                unsafe { _mm256_extract_epi32(self.lasts, 5) },
+                &self.metadata[5] );
+        }
+        if cmp & masks[6] != 6 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 6) },
+                unsafe { _mm256_extract_epi32(self.lasts, 6) },
+                &self.metadata[6] );
+        }
+        if cmp & masks[7] != 7 {
+            visit(
+                unsafe { _mm256_extract_epi32(self.firsts, 7) },
+                unsafe { _mm256_extract_epi32(self.lasts, 7) },
+                &self.metadata[7] );
+        }
+    }
+
     fn query_chunk<F>(&self, query_first: i32x8, query_last: i32x8, mut visit: F)
             where F: FnMut(i32, i32) {
 
@@ -374,11 +434,14 @@ impl<T, I> COITree<T, I> where T: Default + Copy + Clone, I: IntWithMax {
     }
 
     // /// Find intervals in the set overlaping the query `[first, last]` and call `visit` on every overlapping node
-    // pub fn query<'a, F>(&'a self, first: i32, last: i32, mut visit: F) where F: FnMut(&'a IntervalNode<T, I>) {
-    //     if !self.is_empty() {
-    //         query_recursion(&self.nodes, self.root_idx, first, last, &mut visit);
-    //     }
-    // }
+    pub fn query<'a, F>(&'a self, first: i32, last: i32, mut visit: F) where F: FnMut(Interval<&'a T>) {
+        let (firstv, lastv) = unsafe {
+            (_mm256_set1_epi32(first), _mm256_set1_epi32(last)) };
+
+        if !self.is_empty() {
+            query_recursion(&self.nodes, self.root_idx, first, last, firstv, lastv, &mut visit);
+        }
+    }
 
     /// Count the number of intervals in the set overlapping the query `[first, last]`.
     pub fn query_count(&self, first: i32, last: i32) -> usize  {
@@ -518,40 +581,43 @@ impl<T, I> COITree<T, I> where T: Default + Copy + Clone, I: IntWithMax {
 
 // // Recursively count overlaps between the tree specified by `nodes` and a
 // // query interval specified by `first`, `last`.
-// fn query_recursion<'a, T, I, F>(
-//         nodes: &'a [IntervalNode<T, I>], root_idx: usize, first: i32, last: i32,
-//         visit: &mut F) where T: Clone, I: IntWithMax, F: FnMut(&'a IntervalNode<T, I>) {
+fn query_recursion<'a, T, I, F>(
+        nodes: &'a [IntervalNode<T, I>], root_idx: usize, first: i32, last: i32,
+        firstv: i32x8, lastv: i32x8,
+        visit: &mut F) where T: Clone, I: IntWithMax, F: FnMut(Interval<&'a T>) {
 
-//     let node = &nodes[root_idx];
+    let node = &nodes[root_idx];
 
-//     if node.left == node.right { // simple subtree
-//         for node in &nodes[root_idx..root_idx + node.right.to_usize()] {
-//             if last < node.first {
-//                 break;
-//             } else if first <= node.last {
-//                 visit(node);
-//             }
-//         }
-//     } else {
-//         if overlaps(node.first, node.last, first, last) {
-//             visit(node);
-//         }
+    if node.left == node.right { // simple subtree
+        for node in &nodes[root_idx..root_idx + node.right.to_usize()] {
+            if last < node.min_first() {
+                break;
+            }
 
-//         if node.left < I::MAX {
-//             let left = node.left.to_usize();
-//             if nodes[left].subtree_last >= first {
-//                 query_recursion(nodes, left, first, last, visit);
-//             }
-//         }
+            node.query_chunk_metadata(firstv, lastv, |first_hit, last_hit, metadata| {
+                visit(Interval{first: first_hit, last: last_hit, metadata: metadata})
+            });
+        }
+    } else {
+        node.query_chunk_metadata(firstv, lastv, |first_hit, last_hit, metadata| {
+            visit(Interval{first: first_hit, last: last_hit, metadata: metadata})
+        });
 
-//         if node.right < I::MAX {
-//             let right = node.right.to_usize();
-//             if overlaps(node.first, nodes[right].subtree_last, first, last) {
-//                 query_recursion(nodes, right, first, last, visit);
-//             }
-//         }
-//     }
-// }
+        if node.left < I::MAX {
+            let left = node.left.to_usize();
+            if nodes[left].subtree_last >= first {
+                query_recursion(nodes, left, first, last, firstv, lastv, visit);
+            }
+        }
+
+        if node.right < I::MAX {
+            let right = node.right.to_usize();
+            if overlaps(node.min_first(), nodes[right].subtree_last, first, last) {
+                query_recursion(nodes, right, first, last, firstv, lastv, visit);
+            }
+        }
+    }
+}
 
 
 // query_recursion but just count number of overlaps
